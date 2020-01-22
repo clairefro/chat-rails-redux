c6b3f6e16d660407f19de17606497e3c7dbfbc83
claire froelich <claire.froelich@gmail.com>
2020-01-22 18:15:41 -0500

    add seeds

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,4 +1,4 @@
 class Channel < ApplicationRecord
-  validates: :name, presence: true
-  has_many :messages
+  validates :name, presence: true
+  has_many :messages, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# DESTORY MESSAGE/USERS/CHANNELS
+puts "destorying messages, users, channels..."
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+
+puts "creating channels..."
+channel = Channel.create({name:'general'})
+Channel.create({name:'paris'})
+Channel.create({name:'montreal'})
+Channel.create({name:'react'})
+Channel.create({name:'help'})
+
+puts "creating sample users..."
+blair = User.create({email: "blair@example.com", password:"aaaaaa"})
+example = User.create({email: "example@example.com", password:"aaaaaa"})
+
+puts "creating messages in general channel..."
+Message.new({content:"hello world", user: blair, channel: channel})
+Message.new({content:"I'm not a bot", user: example, channel: channel})
+
+puts "done"
