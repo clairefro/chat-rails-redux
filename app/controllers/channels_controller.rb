ff7b88816835fbd1fa85271ddb9a511886119650
claire froelich <claire.froelich@gmail.com>
2020-01-22 20:58:03 -0500

    messages rendering

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -3,8 +3,12 @@ class ChannelsController < ApplicationController
     if params[:id].blank?
       redirect_to channel_path(Channel.first.name)
     else
-      @channel = Channel.find_by(name: params[:id])
-      @channels = Channel.all
+      channel = Channel.find_by(name: params[:id])
+      channels = Channel.all
     end
+    render json: channel.messages
+  end
+
+  def index
   end
 end
